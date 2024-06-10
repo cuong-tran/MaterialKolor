47e4367ceff2f6e9bab3c86c4837caa54e3a9407
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-06-11 02:11:35 +0700

    using JDK 17

--- material-color-utilities/build.gradle.kts
+++ material-color-utilities/build.gradle.kts
@@ -67,11 +67,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
-        jvmToolchain(jdkVersion = 11)
+        jvmToolchain(jdkVersion = 17)
     }
 }
